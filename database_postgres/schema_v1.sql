c18a81c04e58ba8b562ee3c12c7387b7b69a9602
sebastian6551 <sebastian6551@live.com>
2022-08-05 19:00:05 -0500

    DB deleted

--- database_postgres/schema_v1.sql
+++ database_postgres/schema_v1.sql
@@ -13,10 +13,3 @@ CREATE DATABASE mande_db
     TEMPLATE template0;
 
 \c mande_db
-
-CREATE TABLE usuario(
-	id_usuario SERIAL PRIMARY KEY,
-	nombre_usuario VARCHAR(60) UNIQUE,
-	password VARCHAR(60) CHECK (length(password) > 3),
-	premium BOOLEAN DEFAULT FALSE
-);
