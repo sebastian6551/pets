8dab57df46bb74a61aca251d955f6884f50fee5e
sebastian6551 <sebastian6551@live.com>
2022-08-05 19:16:15 -0500

    New db schema

--- database_postgres/schema_v1.sql
+++ database_postgres/schema_v1.sql
@@ -13,3 +13,25 @@ CREATE DATABASE mande_db
     TEMPLATE template0;
 
 \c mande_db
+
+CREATE TABLE type(
+	type_id serial PRIMARY KEY,
+	type varchar(60),
+	breed varchar(60),
+	description varchar(60)
+);
+
+CREATE TABLE pet(
+	pid serial PRIMARY KEY,
+	pet_name varchar(60),
+	age integer,
+    type_id integer references type
+);
+
+
+CREATE TABLE toy(
+	tid serial PRIMARY KEY,
+	name varchar(60),
+	color varchar(60),
+    pet_id integer references pet
+);
