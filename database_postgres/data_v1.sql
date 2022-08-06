70a36f7270794c345fcd8de2dd4c89b72fac2e3d
sebastian6551 <sebastian6551@live.com>
2022-08-05 19:35:03 -0500

    Some data inserted

--- database_postgres/data_v1.sql
+++ database_postgres/data_v1.sql
@@ -1,1 +1,27 @@
 \c mande_db
+INSERT INTO type VALUES (1, 'Perro', 'Labrador', 'De buena disposición y ánimo. Muy ágil');
+INSERT INTO type VALUES (2, 'Gato', 'Ragdoll', 'Destaca por su pelaje suave');
+
+/* Cats */
+INSERT INTO pet VALUES (default, 'Ángel', 3, 2);
+INSERT INTO pet VALUES (default, 'Kitty', 5, 2);
+INSERT INTO pet VALUES (default, 'Sam', 2, 2);
+INSERT INTO pet VALUES (default, 'Hugo', 4, 2);
+INSERT INTO pet VALUES (default, 'Misty', 1, 2);
+
+/* Dogs */
+INSERT INTO pet VALUES (default, 'Tommy', 2, 1);
+INSERT INTO pet VALUES (default, 'Danger', 1, 1);
+INSERT INTO pet VALUES (default, 'Tito', 4, 1);
+INSERT INTO pet VALUES (default, 'Tony', 1, 1);
+INSERT INTO pet VALUES (default, 'Pipe', 2, 1);
+
+/* Toys */
+INSERT INTO toy VALUES (default, 'Bola1', 'Rojo', 1);
+INSERT INTO toy VALUES (default, 'Cadena', 'Gris', 2);
+INSERT INTO toy VALUES (default, 'Ratón1', 'Gris', 1);
+INSERT INTO toy VALUES (default, 'Ratón2', 'Marrón', 3);
+INSERT INTO toy VALUES (default, 'Bola2', 'Azul', 4);
+INSERT INTO toy VALUES (default, 'Gallo1', 'Blanco', 8);
+INSERT INTO toy VALUES (default, 'Peluza1', 'Verde', 9);
+INSERT INTO toy VALUES (default, 'Goma1', 'Amarillo', 7);
